1c209c0f58878b387d6b86e0c17f42d869978578
mdorf <mdorf@stanford.edu>
2025-07-07 15:50:59 -0700

    re-implemented the fix_datatypes script to correct ALL ontologies, not just those with errors

--- bin/migrations/fix_datatypes.rb
+++ bin/migrations/fix_datatypes.rb
@@ -11,17 +11,17 @@ Contact = LinkedData::Models::Contact
 # ont_acronyms = ["BAO", "CDAO", "CMPO", "DOID", "EMAPA", "GPML", "LABO", "MF", "MI", "MMO", "OHD", "RDA-ISSUANCE"]
 # ont_acronyms = ["RDA-ISSUANCE"]
 # ont_acronyms = ["BAO"]
-# ont_acronyms = ["MI"]
 # ont_acronyms = ["WHO"]
 # ont_acronyms = ["MEDDRA"]
 # ont_acronyms = ["WINDENERGY", "MEDDRA"]
-# ont_acronyms = ["MMO"]
+# ont_acronyms = ["WHOFRE"]
 # ont_acronyms = ["MF"]
 # ont_acronyms = ["ONTODM-CORE"]
 # ont_acronyms = ["FAST-FORMGENRE"]
 # ont_acronyms = ["ALLERGYDETECTOR"]
 # ont_acronyms = ["WETAXTOPICS"]
 # ont_acronyms = ["H1_NMOABA_4_1"]
+# ont_acronyms = ["XAO"]
 
 onts = []
 SPACER = "\n\n\n"
@@ -30,13 +30,45 @@ UNKNOWN_CONTACT = {
   email: "noreply@bioontology.org"
 }
 DEFAULT_URI = "http://www.example.com"
-KNOWN_PREDICATES = {
-  contact: RDF::URI.new("#{Goo.namespaces[:metadata].to_s}contact"),
-  documentation: RDF::URI.new("#{Goo.namespaces[:omv].to_s}documentation"),
-  homepage: RDF::URI.new("#{Goo.namespaces[:metadata].to_s}homepage"),
-  publication: RDF::URI.new("#{Goo.namespaces[:metadata].to_s}publication"),
-  uri: RDF::URI.new("#{Goo.namespaces[:omv].to_s}uri")
+ATTRIBUTES_TO_FIX = {
+  documentation: {
+    predicate: RDF::URI.new("#{Goo.namespaces[:omv].to_s}documentation"),
+    is_single: true
+  },
+  homepage: {
+    predicate: RDF::URI.new("#{Goo.namespaces[:metadata].to_s}homepage"),
+    is_single: true
+  },
+  publication: {
+    predicate: RDF::URI.new("#{Goo.namespaces[:metadata].to_s}publication"),
+    is_single: false
+  },
+  uri: {
+    predicate: RDF::URI.new("#{Goo.namespaces[:omv].to_s}uri"),
+    is_single: true
+  }
 }
+
+def ensure_valid_does_not_crash(sub, logger:)
+  begin
+    sub.valid?
+  rescue ArgumentError => e
+    if (match = e.message.match(/File path (.+?) not found/))
+      missing_path = match[1]
+
+      begin
+        FileUtils.mkdir_p(File.dirname(missing_path))
+        File.write(missing_path, "")
+
+        logger.warn("Created missing file: #{missing_path} to allow validation to proceed")
+      rescue => file_error
+        logger.error("Failed to create missing file #{missing_path}: #{file_error.message}")
+      end
+    else
+      logger.error("Unhandled validation error: #{e.class} - #{e.message}")
+    end
+  end
+end
 
 def fix_invalid_uri(value, logger:, default_scheme: "http", fallback_uri: DEFAULT_URI)
   if value.nil? || value.to_s.strip.empty?
@@ -60,152 +92,58 @@ def fix_invalid_uri(value, logger:, default_scheme: "http", fallback_uri: DEFAUL
   RDF::URI.new(fallback_uri)
 end
 
-def additional_datatype_fix(type, value, logger)
-  case type.to_s
-  when "RDF::URI"
-    value = fix_invalid_uri(value, logger: logger)
-  else
-    # type code here
-  end
-  value
-end
-
-def ensure_valid_does_not_crash(sub, logger:)
-  begin
-    sub.valid?
-  rescue ArgumentError => e
-    if (match = e.message.match(/File path (.+?) not found/))
-      missing_path = match[1]
-
-      begin
-        FileUtils.mkdir_p(File.dirname(missing_path))
-        File.write(missing_path, "")
-
-        logger.warn("Created missing file: #{missing_path} to allow validation to proceed")
-      rescue => file_error
-        logger.error("Failed to create missing file #{missing_path}: #{file_error.message}")
-      end
-    else
-      logger.error("Unhandled validation error: #{e.class} - #{e.message}")
-    end
-  end
-end
-
-def resolve_klass(target_type, logger)
-  klass = Object.const_get(target_type.split("::").first)
-  target_type.split("::")[1..].each { |const| klass = klass.const_get(const) }
-  klass
-rescue NameError => e
-  logger.error("Could not resolve class #{target_type}: #{e.message}#{SPACER}")
-  nil
-end
-
 def delete_original_value(sub, attribute_name, logger:)
-  query = <<eof
-DELETE {
-  GRAPH <#{sub.graph.to_s}> {
-    <#{sub.id.to_s}> <#{KNOWN_PREDICATES[attribute_name.to_sym].to_s}> ?o .
-  }
-}
-WHERE {
-  GRAPH <#{sub.graph.to_s}> {
-    <#{sub.id.to_s}> <#{KNOWN_PREDICATES[attribute_name.to_sym].to_s}> ?o .
-  }
-}
-eof
+  query = <<~SPARQL
+    DELETE {
+      GRAPH <#{sub.graph.to_s}> {
+        <#{sub.id.to_s}> <#{ATTRIBUTES_TO_FIX[attribute_name.to_sym][:predicate].to_s}> ?o .
+      }
+    }
+    WHERE {
+      GRAPH <#{sub.graph.to_s}> {
+        <#{sub.id.to_s}> <#{ATTRIBUTES_TO_FIX[attribute_name.to_sym][:predicate].to_s}> ?o .
+      }
+    }
+  SPARQL
   Goo.sparql_query_client.update(query)
   logger.info("Deleted original value of #{attribute_name} for #{sub.id.to_s}")
 end
 
-def fix_single_attribute(sub, attribute_name, klass, logger)
-  original_value = sub.send(attribute_name)
-  casted_value = klass.new(original_value)
-  casted_value = additional_datatype_fix(klass, casted_value, logger)
-
-  # Delete original value(s) to avoid duplicates due to a different datatype
-  delete_original_value(sub, attribute_name, logger: logger)
-
-  sub.send("#{attribute_name}=", casted_value)
-  logger.info("Attribute \"#{attribute_name}\". Replaced \"#{original_value}\" with \"#{klass}('#{casted_value}')\"")
-rescue => e
-  logger.error("Error assigning #{attribute_name} to #{klass}: #{e.message}#{SPACER}")
-end
-
-def fix_array_attribute(sub, attribute_name, klass, logger)
-  current_value = sub.send(attribute_name)
-  values = current_value.is_a?(Array) ? current_value : [current_value]
-
-  casted_values = values.map do |val|
-    begin
-      klass.new(val)
-      additional_datatype_fix(klass, val, logger)
-    rescue => e
-      logger.error("Skipping value '#{val}' for #{attribute_name}: #{e.message}")
-      nil
-    end
-  end.compact.uniq
-
-  # Delete original value(s) to avoid duplicates due to a different datatype
-  delete_original_value(sub, attribute_name, logger: logger)
-
-  sub.send("#{attribute_name}=", casted_values)
-  logger.info("Attribute \"#{attribute_name}\". Cast each list value to #{klass}: #{casted_values.inspect}")
-rescue => e
-  logger.error("Error processing array for #{attribute_name}: #{e.message}#{SPACER}")
-end
-
-def fix_existence_error(sub, attr, logger)
-  case attr
-  when :contact
-    contact = Contact.where(email: UNKNOWN_CONTACT[:email]).first
-    contact = Contact.new(UNKNOWN_CONTACT).save unless contact
-    sub.contact = [contact]
-    logger.info("Attribute \"#{attr}\" was empty. Added default contact: \"#{UNKNOWN_CONTACT.inspect}\"")
-  else
-    # type code here
-  end
-end
-
-def process_errors(sub, error_hash, logger)
-  error_hash.each do |attr, value_hash|
-    value_hash.each do |key, message|
-      if key == :existence
-        fix_existence_error(sub, attr, logger)
-      elsif match = message.match(/Attribute `(.+?)` with the value `(.+?)` must be `(.+?)`/)
-        attribute_name = match[1]
-        target_type = match[3]
-        klass = resolve_klass(target_type, logger)
-        fix_single_attribute(sub, attribute_name, klass, logger) if klass
-      elsif match = message.match(/All values in attribute `(.+?)` must be `(.+?)`/)
-        attribute_name = match[1]
-        target_type = match[2]
-        klass = resolve_klass(target_type, logger)
-        fix_array_attribute(sub, attribute_name, klass, logger) if klass
-      end
-    end
-  end
-end
-
-def process_homepage(sub, logger)
-  query = <<eof
-SELECT ?o
-FROM <#{sub.graph.to_s}>
-WHERE {
-  <#{sub.id.to_s}> <#{KNOWN_PREDICATES[:homepage].to_s}> ?o .
-}
-eof
+def process_single_attribute(sub, attribute_name, logger:)
+  is_single = ATTRIBUTES_TO_FIX[attribute_name.to_sym][:is_single]
+  query = <<~SPARQL
+    SELECT ?o
+    FROM <#{sub.graph.to_s}>
+    WHERE {
+      <#{sub.id.to_s}> <#{ATTRIBUTES_TO_FIX[attribute_name.to_sym][:predicate].to_s}> ?o .
+    }
+  SPARQL
   rs = Goo.sparql_query_client.query(query)
+  old_vals = []
+  new_vals = []
+
+  rs.each do |result|
+    old_vals << result[:o]
+    new_vals << fix_invalid_uri(result[:o].to_s, logger: logger)
+    break if ATTRIBUTES_TO_FIX[attribute_name.to_sym][:is_single]
+  end
 
   unless rs.empty?
-    old_val = rs[0][:o].object
-
-    # Delete original value(s) to avoid duplicates due to a different datatype
-    delete_original_value(sub, :homepage, logger: logger)
-
-    new_val = fix_invalid_uri(old_val, logger: logger)
-    sub.homepage = new_val
-    logger.info("Attribute homepage. Replaced \"#{old_val}\" with \"RDF::URI('#{new_val}')\"")
+    casted_val = ATTRIBUTES_TO_FIX[attribute_name.to_sym][:is_single] ? new_vals.first : new_vals.compact.uniq
+    delete_original_value(sub, attribute_name, logger: logger)
+    sub.send("#{attribute_name}=", casted_val)
+    logger.info("Attribute #{attribute_name}. Corrected value: #{casted_val.inspect}')")
   end
+rescue => e
+  logger.error("Error assigning #{attribute_name} to #{casted_val.inspect}: #{e.message}#{SPACER}")
+end
+
+def fix_contact(sub, logger:)
+  return if sub.contact
+  contact = Contact.where(email: UNKNOWN_CONTACT[:email]).first
+  contact = Contact.new(UNKNOWN_CONTACT).save unless contact
+  sub.contact = [contact]
+  logger.info("Contact for #{sub.id.to_s} was empty. Added default contact: \"#{UNKNOWN_CONTACT.inspect}\"")
 end
 
 # ---------------
@@ -241,26 +179,18 @@ onts.each do |ont|
 
     ensure_valid_does_not_crash(sub, logger: logger)
 
+    ATTRIBUTES_TO_FIX.each_key { |name|
+      process_single_attribute(sub, name, logger: logger)
+    }
+
+    fix_contact(sub, logger: logger)
+
     if sub.valid?
-      logger.info("Nothing to fix for #{ont.acronym}#{SPACER}")
-      next
-    end
-
-    error_hash = sub.errors
-    logger.info("Errors: #{error_hash}\n")
-
-    if error_hash && !error_hash.empty?
-      process_errors(sub, error_hash, logger)
-      process_homepage(sub, logger)
-      ensure_valid_does_not_crash(sub, logger: logger)
-
-      if sub.valid?
-        sub.save
-        logger.info("Fixed submission for ontology #{ont.acronym} with id #{sub.id}#{SPACER}")
-      else
-        logger.error("Unable to fix latest submission for ontology #{ont.acronym} with id #{sub.id}: #{sub.errors}#{SPACER}")
-        unfixed_acronyms << ont.acronym
-      end
+      sub.save
+      logger.info("Fixed submission for ontology #{ont.acronym} with id #{sub.id}#{SPACER}")
+    else
+      logger.error("Unable to fix latest submission for ontology #{ont.acronym} with id #{sub.id}: #{sub.errors}#{SPACER}")
+      unfixed_acronyms << ont.acronym
     end
   rescue StandardError => e
     logger.error(e)
