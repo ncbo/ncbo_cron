9489c499a80ac82e379a57e45a772a68af0b7a47
mdorf <mdorf@stanford.edu>
2026-02-04 20:06:57 -0800

    added new processing actions: extract_metadata and index_all_data

--- lib/ncbo_cron/ontology_helper.rb
+++ lib/ncbo_cron/ontology_helper.rb
@@ -9,6 +9,8 @@ module NcboCron
       PROCESS_ACTIONS = {
         :process_rdf => true,
         :generate_labels => true,
+        :extract_metadata => true,
+        :index_all_data => true,
         :index_search => true,
         :index_properties => true,
         :run_metrics => true,
